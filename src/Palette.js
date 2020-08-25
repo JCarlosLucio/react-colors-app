1ab082acba63b8983da45492118266560e4167ac
jcLucio <c.lucio3@gmail.com>
2020-08-24 22:57:21 -0500

    Refactor to functional component

--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Palette extends Component {
-  render() {
-    return (
-      <div className="Palette">
-        {/* Navbar goes here */}
-        <div className="Palette-colores">{/* Color boxes go here */}</div>
-        {/* Footer goes here */}
-      </div>
-    );
-  }
+function Palette() {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Palette-colores">{/* Color boxes go here */}</div>
+      {/* Footer goes here */}
+    </div>
+  );
 }
 
 export default Palette;
