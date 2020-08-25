80a8d9a81bb2894ebc0887c62324811847a9a467
jcLucio <c.lucio3@gmail.com>
2020-08-25 16:26:41 -0500

    Add CopyToClipboard w/useTimedToggle

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import useTimedToggle from './hooks/useTimedToggle';
 import './ColorBox.css';
 
 function ColorBox({ background, name }) {
+  const [isCopied, toggleIsCopied] = useTimedToggle(1500);
+
   return (
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={toggleIsCopied}>
       <div style={{ background }} className="ColorBox">
+        <div
+          style={{ background }}
+          className={`copy-overlay ${isCopied && 'show'}`}
+        />
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
