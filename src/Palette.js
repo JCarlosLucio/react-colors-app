1a35624cb01bf78e8bec573e51a76eaa6fa4405a
jcLucio <c.lucio3@gmail.com>
2020-08-24 21:34:52 -0500

    Add Palette file w/boilerplate

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PALETTE</h1>
+      </div>
+    );
+  }
+}
+
+export default Palette;
