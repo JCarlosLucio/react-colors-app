9a1016ebe28e1edf3e8daf77db0bb32ff622a7b9
jcLucio <c.lucio3@gmail.com>
2020-08-24 21:41:10 -0500

    Create divs and plan

--- src/Palette.js
+++ src/Palette.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export class Palette extends Component {
   render() {
     return (
-      <div>
-        <h1>PALETTE</h1>
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colores">{/* Color boxes go here */}</div>
+        {/* Footer goes here */}
       </div>
     );
   }
