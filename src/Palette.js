0fd94ae453011adcb0cf62e64fe9ec40369d99fc
jcLucio <c.lucio3@gmail.com>
2020-08-26 12:47:17 -0500

    Add Slider w/level state

--- src/Palette.js
+++ src/Palette.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 function Palette({ palette }) {
-  const colorBoxes = palette.colors[500].map((color) => (
+  const [level, setLevel] = useState(500);
+
+  const colorBoxes = palette.colors[level].map((color) => (
     <ColorBox background={color.hex} name={color.name} />
   ));
   return (
     <div className="Palette">
       {/* Navbar goes here */}
-      <Slider />
+      <Slider
+        defaultValue={level}
+        min={100}
+        max={900}
+        step={100}
+        onAfterChange={(newLevel) => setLevel(newLevel)}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer goes here */}
     </div>
