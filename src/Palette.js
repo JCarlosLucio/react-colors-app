30fbab3c44468f00559b470eabbd90527914e7fd
jcLucio <c.lucio3@gmail.com>
2020-08-27 11:09:28 -0500

    Move Slider into div.slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import ColorBox from './ColorBox';
-import './Palette.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Palette.css';
 
 function Palette({ palette }) {
   const [level, setLevel] = useState(500);
@@ -13,13 +13,15 @@ function Palette({ palette }) {
   return (
     <div className="Palette">
       {/* Navbar goes here */}
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        step={100}
-        onAfterChange={(newLevel) => setLevel(newLevel)}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={(newLevel) => setLevel(newLevel)}
+        />
+      </div>
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer goes here */}
     </div>
