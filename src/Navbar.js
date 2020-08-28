5ea7b2294b6a40ecb98c2f3c7eda83c09b29fec5
jcLucio <c.lucio3@gmail.com>
2020-08-28 12:14:33 -0500

    Add span for 'Level: level'

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,21 +8,24 @@ function Navbar({ level, setLevel }) {
       <div className="logo">
         <a href="#">reactcolorapp</a>
       </div>
-      <div className="slider">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={(newLevel) => setLevel(newLevel)}
-          trackStyle={{ backgroundColor: 'transparent' }}
-          handleStyle={{
-            backgroundColor: 'green',
-            borderColor: 'green',
-            marginTop: -3,
-          }}
-          railStyle={{ height: 8 }}
-        />
+      <div className="slider-container">
+        <span>Level: {level}</span>
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={(newLevel) => setLevel(newLevel)}
+            trackStyle={{ backgroundColor: 'transparent' }}
+            handleStyle={{
+              backgroundColor: 'green',
+              borderColor: 'green',
+              marginTop: -3,
+            }}
+            railStyle={{ height: 8 }}
+          />
+        </div>
       </div>
     </nav>
   );
