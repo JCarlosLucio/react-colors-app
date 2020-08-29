50ad6d05bdf3b7f2681301b1adf43bc3277fd86b
jcLucio <c.lucio3@gmail.com>
2020-08-29 17:04:36 -0500

    Add format/setFormat and pass it to Navbar

--- src/Palette.js
+++ src/Palette.js
@@ -5,13 +5,19 @@ import './Palette.css';
 
 function Palette({ palette }) {
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState('hex');
 
   const colorBoxes = palette.colors[level].map((color) => (
     <ColorBox background={color.hex} name={color.name} />
   ));
   return (
     <div className="Palette">
-      <Navbar level={level} setLevel={setLevel} />
+      <Navbar
+        level={level}
+        setLevel={setLevel}
+        format={format}
+        setFormat={setFormat}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer goes here */}
     </div>
