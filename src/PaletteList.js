c6527cd5ce6f059c41edcd605899d0892009d4c4
jcLucio <c.lucio3@gmail.com>
2020-09-01 23:17:37 -0500

    Add palettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function PaletteList() {
-  return <div />;
+function PaletteList({ palettes }) {
+  return (
+    <div>
+      <h1>React Colors</h1>
+      {palettes.map((palette) => <p>{palette.paletteName}</p>)}
+    </div>
+  );
 }
 
 export default PaletteList;
