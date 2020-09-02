da782f984a94b67b1c6e7cd87c25bc4ff997530a
jcLucio <c.lucio3@gmail.com>
2020-09-01 23:09:38 -0500

    Import PaletteList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './helpers/colorHelpers';
 
