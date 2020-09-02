db829856f062e1b371e5bdb0f6856fa629b9b1f2
jcLucio <c.lucio3@gmail.com>
2020-09-02 12:37:08 -0500

    Update styles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
-  root: {},
-  colors: {},
-  title: {},
-  emoji: {},
+  root: {
+    backgroundColor: 'white',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  colors: {
+    backgroundColor: 'grey',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '1rem',
+    position: 'relative',
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem',
+  },
 };
 
 function MiniPalette({ classes, paletteName, emoji, id, colors }) {
