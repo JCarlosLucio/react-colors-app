0ef0b1ff9f6985cbaa08a3961755861978dd9a1e
jcLucio <c.lucio3@gmail.com>
2020-09-04 11:43:43 -0500

    Add styles for LevelSlider

--- src/LevelSlider.js
+++ src/LevelSlider.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
+import { withStyles } from '@material-ui/core/styles';
 
-function LevelSlider() {
-  return <div />;
-}
+const styles = {
+  root: {
+    color: '#d4d5d9',
+    height: 8,
+  },
+  thumb: {
+    height: 16,
+    width: 16,
+    backgroundColor: 'green',
+    border: '2px solid green',
+    marginTop: -4,
+    marginLeft: -8,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+};
+
+const LevelSlider = withStyles(styles)(Slider);
 
 export default LevelSlider;
