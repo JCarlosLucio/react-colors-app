43a91983aa39093f315ae9223e256956ccc8c69d
jcLucio <c.lucio3@gmail.com>
2020-09-07 10:25:26 -0500

    Add initial Route for single color page

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
           />
         )}
       />
+      <Route
+        exact
+        path="/palette/:paletteId/:colorId"
+        render={() => <h1>SINGLE COLOR PAGE</h1>}
+      />
     </Switch>
   );
 }
