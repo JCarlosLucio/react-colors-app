67d1b9bd9d10fd5ba49af3181653ccac818a7056
jcLucio <c.lucio3@gmail.com>
2020-09-08 22:49:12 -0500

    Add gatherShades/shades

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
+const gatherShades = (palette, colorId) => {
+  const shades = [];
+  const allColors = palette.colors;
+  for (const key in allColors) {
+    shades.push(allColors[key].find((color) => color.id === colorId));
+  }
+  return shades.slice(1);
+};
+
 function SingleColorPalette({ palette, colorId }) {
+  const shades = gatherShades(palette, colorId);
+
   return (
     <div>
       <h1>SINGLE COLOR PALETTE</h1>
