e1ae93ad264ec041a6768e95895d36e84f6fe800
jcLucio <c.lucio3@gmail.com>
2020-09-15 21:24:23 -0500

    Refactor Palette-footer styles to use wtihStyles

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = {};
+const styles = {
+  PaletteFooter: {
+    backgroundColor: 'white',
+    height: '5vh',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    fontWeight: 'bold',
+  },
+  emoji: {
+    fontSize: '1.5rem',
+    margin: '0 1rem',
+  },
+};
 
-function PaletteFooter({ paletteName, emoji }) {
+function PaletteFooter({ paletteName, emoji, classes }) {
   return (
-    <footer className="Palette-footer">
+    <footer className={classes.PaletteFooter}>
       {paletteName}
-      <span className="emoji">{emoji}</span>
+      <span className={classes.emoji}>{emoji}</span>
     </footer>
   );
 }
