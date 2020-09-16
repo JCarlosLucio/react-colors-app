404bd466fea63b0aea7a230c1280fe9aeae11fba
jcLucio <c.lucio3@gmail.com>
2020-09-15 20:07:55 -0500

    Refactor goBack style

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -23,6 +23,35 @@ const styles = {
   colors: {
     height: '90%',
   },
+  goBack: {
+    width: '20%',
+    height: '50%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-4px',
+    background: 'black',
+    '& a': {
+      color: 'white',
+      width: '100px',
+      height: '30px',
+      position: 'absolute',
+      display: 'inline-block',
+      top: '50%',
+      left: '50%',
+      marginLeft: '-50px',
+      marginTop: '-15px',
+      textAlign: 'center',
+      outline: 'none',
+      background: 'rgba(255, 255, 255, 0.3)',
+      fontSize: '1rem',
+      lineHeight: '30px',
+      textTransform: 'uppercase',
+      textDecoration: 'none',
+      border: 'none',
+    },
+  },
 };
 
 function SingleColorPalette({ palette, colorId, classes }) {
@@ -43,10 +72,8 @@ function SingleColorPalette({ palette, colorId, classes }) {
       <Navbar format={format} setFormat={setFormat} showingAllColors={false} />
       <div className={classes.colors}>
         {colorBoxes}
-        <div className="go-back ColorBox">
-          <Link to={`/palette/${palette.id}`} className="back-button">
-            GO BACK
-          </Link>
+        <div className={classes.goBack}>
+          <Link to={`/palette/${palette.id}`}>GO BACK</Link>
         </div>
       </div>
       <PaletteFooter paletteName={palette.paletteName} emoji={palette.emoji} />
