9795ca033494537d1801600e3c31ca1766888aa6
jcLucio <c.lucio3@gmail.com>
2020-09-17 18:41:59 -0500

    Add Route for NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import PaletteList from './PaletteList';
 import Palette from './Palette';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 import seedColors from './seedColors';
 import { generatePalette } from './helpers/colorHelpers';
 
@@ -18,6 +19,7 @@ function App() {
           <PaletteList palettes={seedColors} {...routeProps} />
         )}
       />
+      <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
       <Route
         exact
         path="/palette/:id"
