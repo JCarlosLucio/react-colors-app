9399a0ea2d0db4d16af3b7ad3d019115d610e617
jcLucio <c.lucio3@gmail.com>
2020-09-17 18:42:51 -0500

    Add Link for 'Create Palette'

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/PaletteListStyles';
@@ -12,6 +13,7 @@ function PaletteList({ classes, palettes, history }) {
       <div className={classes.container}>
         <nav className={classes.nav}>
           <h1>React Colors App</h1>
+          <Link to="/palette/new">Create Palette</Link>
         </nav>
         <div className={classes.palettes}>
           {palettes.map((palette) => (
