7ff9de9780ba63e6e50c7cc53a5a2f2f7a8051e4
jcLucio <c.lucio3@gmail.com>
2020-09-24 19:05:47 -0500

    Refactor DraggableColorBox Components to DraggableColorList

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import DraggableColorBox from './DraggableColorBox';
 import { SortableContainer } from 'react-sortable-hoc';
 
-function DraggableColorList() {
-  return <div />;
+function DraggableColorList({ colors, removeColor }) {
+  return (
+    <div style={{ height: '100%' }}>
+      {colors.map((color) => (
+        <DraggableColorBox
+          key={color.name}
+          color={color.color}
+          name={color.name}
+          handleClick={() => removeColor(color.name)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default DraggableColorList;
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -13,7 +13,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import DraggableColorBox from './DraggableColorBox';
+import DraggableColorList from './DraggableColorList';
 
 const drawerWidth = 400;
 
@@ -226,14 +226,7 @@ function NewPaletteForm({ palettes, savePalette, history, classes }) {
         })}
       >
         <div className={classes.drawerHeader} />
-        {colors.map((color) => (
-          <DraggableColorBox
-            key={color.name}
-            color={color.color}
-            name={color.name}
-            handleClick={() => removeColor(color.name)}
-          />
-        ))}
+        <DraggableColorList colors={colors} removeColor={removeColor} />
       </main>
     </div>
   );
