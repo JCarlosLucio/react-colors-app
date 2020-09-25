389a78517511e1eaec427650e2c676623983df7c
jcLucio <c.lucio3@gmail.com>
2020-09-24 19:59:26 -0500

    Add index prop to DraggableColorBox

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -5,8 +5,9 @@ import { SortableContainer } from 'react-sortable-hoc';
 const DraggableColorList = SortableContainer(({ colors, removeColor }) => {
   return (
     <div style={{ height: '100%' }}>
-      {colors.map((color) => (
+      {colors.map((color, i) => (
         <DraggableColorBox
+          index={i}
           key={color.name}
           color={color.color}
           name={color.name}
