b2646c19e9d1b7f65658df60f0dfcc8b5cc772d7
jcLucio <c.lucio3@gmail.com>
2020-09-28 16:42:44 -0500

    Update drawerHeader style

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -30,6 +30,7 @@ const styles = (theme) => ({
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
+    width: '100%',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
