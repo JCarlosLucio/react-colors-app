59da9ce4f83bc13f9b8f1bb19267aafa7ad36698
jcLucio <c.lucio3@gmail.com>
2020-09-28 15:47:05 -0500

    Add withStyles to ColorPickerForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
+import { withStyles } from '@material-ui/core/styles';
 import useInputState from './hooks/useInputState';
 import Button from '@material-ui/core/Button';
+
+const styles = {};
 
 function ColorPickerForm({ isPaletteFull, addNewColor, colors }) {
   const [currentColor, setCurrentColor] = useState('teal');
@@ -60,4 +63,4 @@ function ColorPickerForm({ isPaletteFull, addNewColor, colors }) {
   );
 }
 
-export default ColorPickerForm;
+export default withStyles(styles)(ColorPickerForm);
