3eea78382385ad7379e397419724fc5f794946b0
jcLucio <c.lucio3@gmail.com>
2020-10-05 10:53:14 -0500

    Update media query helper 'sizes' with up

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -1,5 +1,17 @@
+// Sizes taken from https://getbootstrap.com/docs/4.5/layout/overview/
+
 export default {
-  up() {},
+  // usually used when mobile first
+  up(size) {
+    const sizes = {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    };
+    return `@media (min-width: ${sizes[size]})`;
+  },
+  // usually used when NOT mobile first
   down(size) {
     const sizes = {
       xs: '575.98px',
