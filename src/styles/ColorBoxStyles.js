d98797ded901877b8e16be508360b8ad79907351
jcLucio <c.lucio3@gmail.com>
2020-10-05 11:44:30 -0500

    Update ColorBox responsive height style

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -16,11 +16,12 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: ({ showingFullPalette }) => (showingFullPalette ? '25%' : '50%'),
+      height: ({ showingFullPalette }) =>
+        showingFullPalette ? '20%' : '33.3333%',
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: ({ showingFullPalette }) => (showingFullPalette ? '20%' : '50%'),
+      height: ({ showingFullPalette }) => (showingFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
