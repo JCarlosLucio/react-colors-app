673bffe7a390fbe3f408a49db92f3d2ebeb0c9bd
jcLucio <c.lucio3@gmail.com>
2020-10-05 11:14:28 -0500

    Update ColorBox style width to be responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js';
+import sizes from './sizes';
 
 export default {
   ColorBox: {
@@ -12,6 +13,18 @@ export default {
     '&:hover button': {
       opacity: 1,
       transition: '0.5s',
+    },
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: ({ showingFullPalette }) => (showingFullPalette ? '25%' : '50%'),
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: ({ showingFullPalette }) => (showingFullPalette ? '20%' : '50%'),
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: ({ showingFullPalette }) => (showingFullPalette ? '5%' : '10%'),
     },
   },
   copyText: {
