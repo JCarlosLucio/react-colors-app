b2f3aa0d7a0976e5380d110259a2793cd6dd7aea
jcLucio <c.lucio3@gmail.com>
2020-10-05 11:45:25 -0500

    Update goBack width/height style to be responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   Palette: {
     display: 'flex',
@@ -34,6 +36,18 @@ export default {
       textTransform: 'uppercase',
       textDecoration: 'none',
       border: 'none',
+    },
+    [sizes.down('lg')]: {
+      width: '75%',
+      height: '33.3333%',
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: '20%',
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%',
     },
   },
 };
