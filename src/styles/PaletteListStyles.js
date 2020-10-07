0c831f402bac433233ddbd3f7a0f46522d709671
jcLucio <c.lucio3@gmail.com>
2020-10-07 14:08:18 -0500

    Add responsive styles to palettes grid

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   root: {
     height: '100vh',
@@ -27,7 +29,14 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%',
+    gridGap: '2.5rem',
     boxSizing: 'border-box',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1.5rem',
+    },
   },
 };
